f46a53ce45855b848f2151479e2061031bfc849e
valeriy.knyazhev <valeriy.knyazhev@yandex.ru>
2017-08-23 00:17:01 +0300

    добавлены пулы задач

---
+++ executor/task/task.go
@@ -0,0 +1,26 @@
+package task
+
+import "sync"
+
+type RecognizeTask struct {
+	RequestId  int
+	Wg         sync.WaitGroup
+	PictureUrl string
+	LangTo     string
+}
+
+type GrammarCheckTask struct {
+	RequestId      int
+	Wg             sync.WaitGroup
+	RecognizedText string
+	LangFrom       string
+	LangTo         string
+}
+
+type TranslateTask struct {
+	RequestId   int
+	Wg          sync.WaitGroup
+	CheckedText string
+	LangFrom    string
+	LangTo      string
+}
