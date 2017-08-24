0cea739782472953c8d1f07e2231f407921b36c9
bird <bird@bird.ru>
2017-08-24 04:23:16 +0300

    new functon for loading image on server has been added

--- aitserver/aitserver.go
+++ aitserver/aitserver.go
@@ -9,10 +9,14 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/satori/go.uuid"
+	log "github.com/sirupsen/logrus"
+	"io"
 	"io/ioutil"
-	"log"
+	"mime"
+	"mime/multipart"
 	"net/http"
 	"os"
+	"strings"
 )
 
 type aitServerLog struct {
@@ -27,12 +31,16 @@ func newLogger(logName string) *aitServerLog {
 
 func (logServ *aitServerLog) creatLogger() error {
 	var err error
-	logServ.logFile, err = os.OpenFile(logServ.logFileName, os.O_WRONLY | os.O_CREATE, 0666)
+	logServ.logFile, err = os.OpenFile(logServ.logFileName, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
+		log.Println("creat logger Error: ", err)
 		return err
 	}
 
-	logServ.logObj = log.New(logServ.logFile, "AitHTTPServer: ", log.Lshortfile)
+	logServ.logObj = log.New()
+
+	logServ.logObj.Out = logServ.logFile
+	logServ.logObj.SetLevel(log.DebugLevel)
 
 	return nil
 }
@@ -66,7 +74,7 @@ func (servConf *AitHTTPServer) initServer() error {
 	var err error
 	servConf.ServerConfig, err = configuration.ReadConfig()
 	if err != nil {
-		log.Println("Config Error: ", err)
+		log.Println("Init server Error: ", err)
 		return err
 	}
 
@@ -74,7 +82,7 @@ func (servConf *AitHTTPServer) initServer() error {
 
 	err = servConf.ServerLogger.creatLogger()
 	if err != nil {
-		log.Println("newLogger Error: ", err)
+		log.Println("Init server Error: ", err)
 		return err
 	}
 
@@ -100,6 +108,7 @@ func (servConf *AitHTTPServer) initServer() error {
 func (servConf *AitHTTPServer) stopServer() error {
 	err := servConf.ServerLogger.closeLogger()
 	if err != nil {
+		log.Println("Stop server Error: ", err)
 		return err
 	}
 	return nil
@@ -117,40 +126,140 @@ type serverAcceptedResponse struct {
 
 type serverRequsetURLPost struct {
 	PictureUrl string `json:"pictureUrl"`
-	LangFrom   string `json:"langFrom"`
-	LangTo     string `json:"langTo"`
 }
 
+const (
+	HeaderLangFrom = "Img-Lang-From"
+	HeaderLangTo   = "Img-Lang-To"
+	HeaderContType = "Content-Type"
+	JsonContType   = "application/json"
+	OctetsContType = "multipart/"
+)
+
 func (servConf AitHTTPServer) makeResponse(w http.ResponseWriter, code int, body interface{}) {
-	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Content-Type", JsonContType)
 	w.WriteHeader(code)
 	err := json.NewEncoder(w).Encode(body)
 	if err != nil {
 		servConf.ServerLogger.logObj.Println(err)
-
 	}
 }
 
 func (servConf AitHTTPServer) CreatNewTranslationTask(w http.ResponseWriter, req *http.Request) {
 
-        log.Println("New connect for POST")
-
-	reqBody, err := ioutil.ReadAll(req.Body)
-	if err != nil {
-		servConf.ServerLogger.logObj.Println(err)
-		servConf.makeResponse(w, http.StatusInternalServerError, serverErrorResponse{Code: http.StatusText(http.StatusInternalServerError), Info: fmt.Sprint(err)})
-		return
-	}
-
-	var reqURLimg serverRequsetURLPost
-	err = json.Unmarshal(reqBody, &reqURLimg)
-	if err != nil {
-		servConf.ServerLogger.logObj.Println(err)
-		servConf.makeResponse(w, http.StatusInternalServerError, serverErrorResponse{Code: http.StatusText(http.StatusInternalServerError), Info: fmt.Sprint(err)})
-		return
-	}
-
+	log.Println("New connect with POST")
 	uuid := uuid.NewV4()
+
+	langFrom := req.Header.Get(HeaderLangFrom)
+
+	if langFrom == "" {
+		servConf.makeResponse(w, http.StatusBadRequest,
+			serverErrorResponse{Code: http.StatusText(http.StatusBadRequest),
+				Info: "Header Img-Lang-From wasm't found"})
+		return
+	}
+
+	langTo := req.Header.Get(HeaderLangTo)
+	if langTo == "" {
+		servConf.makeResponse(w, http.StatusBadRequest,
+			serverErrorResponse{Code: http.StatusText(http.StatusBadRequest),
+				Info: "Header Img-Lang-To wasm't found"})
+		return
+	}
+	contType, params, err := mime.ParseMediaType(req.Header.Get(HeaderContType))
+	if err != nil {
+		servConf.ServerLogger.logObj.Println(err)
+		servConf.makeResponse(w, http.StatusInternalServerError,
+			serverErrorResponse{Code: http.StatusText(http.StatusInternalServerError),
+				Info: fmt.Sprint(err)})
+		return
+	}
+	if contType == "" {
+		servConf.makeResponse(w, http.StatusBadRequest,
+			serverErrorResponse{Code: http.StatusText(http.StatusBadRequest),
+				Info: "Header Content-Type wasm't found"})
+		return
+	}
+
+	var pathToImg string
+
+	if strings.HasPrefix(contType, OctetsContType) {
+		pathToImg = fmt.Sprintf("%s.img", uuid)
+		var imgFile *os.File
+		imgFile, err = os.OpenFile(pathToImg, os.O_WRONLY|os.O_CREATE, 0666)
+		defer imgFile.Close()
+		if err != nil {
+			servConf.ServerLogger.logObj.Println(err)
+			servConf.makeResponse(w, http.StatusInternalServerError,
+				serverErrorResponse{Code: http.StatusText(http.StatusInternalServerError),
+					Info: fmt.Sprint(err)})
+			return
+		}
+		pathToImg = "file://" + pathToImg
+
+		mr := multipart.NewReader(req.Body, params["boundary"])
+
+		for {
+			npart, err := mr.NextPart()
+			if err == io.EOF {
+
+				err = nil
+
+				break
+			}
+			if err != nil {
+				servConf.ServerLogger.logObj.Println(err)
+				servConf.makeResponse(w, http.StatusInternalServerError,
+					serverErrorResponse{Code: http.StatusText(http.StatusInternalServerError),
+						Info: fmt.Sprint(err)})
+				return
+			}
+			slurp, err := ioutil.ReadAll(npart)
+			if err != nil {
+				servConf.ServerLogger.logObj.Println(err)
+				servConf.makeResponse(w, http.StatusInternalServerError,
+					serverErrorResponse{Code: http.StatusText(http.StatusInternalServerError),
+						Info: fmt.Sprint(err)})
+				return
+			}
+			_, err = imgFile.Write(slurp)
+			if err != nil {
+				servConf.ServerLogger.logObj.Println(err)
+				servConf.makeResponse(w, http.StatusInternalServerError,
+					serverErrorResponse{Code: http.StatusText(http.StatusInternalServerError),
+						Info: fmt.Sprint(err)})
+				return
+			}
+		}
+
+	} else if contType == JsonContType {
+
+		var reqURLimg serverRequsetURLPost
+
+		reqBody, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			servConf.ServerLogger.logObj.Println(err)
+			servConf.makeResponse(w, http.StatusInternalServerError,
+				serverErrorResponse{Code: http.StatusText(http.StatusInternalServerError),
+					Info: fmt.Sprint(err)})
+			return
+		}
+		err = json.Unmarshal(reqBody, &reqURLimg)
+		if err != nil {
+			servConf.ServerLogger.logObj.Println(err)
+			servConf.makeResponse(w, http.StatusInternalServerError,
+				serverErrorResponse{Code: http.StatusText(http.StatusInternalServerError),
+					Info: fmt.Sprint(err)})
+			return
+		}
+		pathToImg = reqURLimg.PictureUrl
+	} else {
+		servConf.makeResponse(w, http.StatusBadRequest,
+			serverErrorResponse{Code: http.StatusText(http.StatusBadRequest),
+				Info: "Invalide content-type"})
+		return
+	}
+
 	/*
 	   insert to DB
 	*/
@@ -160,11 +269,17 @@ func (servConf AitHTTPServer) CreatNewTranslationTask(w http.ResponseWriter, req
 	   Start Task
 
 	*/
-	//test { "pictureUrl": "https://www.w3.org/TR/SVGTiny12/examples/textArea01.png", "langFrom": "en", "langTo" : "ru" } 
-	TEXT, _ := servConf.ServerVision.GetTextFromImg(reqURLimg.PictureUrl, vision.UrlPathType, vision.OcrImgType, reqURLimg.LangFrom)
+	//test { "pictureUrl": "https://www.w3.org/TR/SVGTiny12/examples/textArea01.png", "langFrom": "en", "langTo" : "ru" }
+	TEXT, err := servConf.ServerVision.GetTextFromImg(pathToImg, langFrom)
 
+	if err != nil {
+		servConf.makeResponse(w, http.StatusBadRequest,
+			serverErrorResponse{Code: http.StatusText(http.StatusBadRequest),
+				Info: fmt.Sprint(err)})
+		return
+	}
 	servConf.makeResponse(w, http.StatusAccepted, serverAcceptedResponse{Info: TEXT.Text, Id: string(uuid[:])})
-//	servConf.makeResponse(w, http.StatusAccepted, serverAcceptedResponse{Info: "For future translation use request Id", Id: string(uuid[:])})
+	//	servConf.makeResponse(w, http.StatusAccepted, serverAcceptedResponse{Info: "For future translation use request Id", Id: string(uuid[:])})
 
 }
 
@@ -176,7 +291,7 @@ type serverOKResponseForGet struct {
 
 func (servConf AitHTTPServer) GetTranslationResult(w http.ResponseWriter, req *http.Request) {
 
-        log.Println("New connect for GET")
+	log.Println("New connect for GET")
 	params := mux.Vars(req)
 	taskID := params["id"]
 
@@ -208,12 +323,12 @@ func (servConf *AitHTTPServer) RunHTTPServer() error {
 
 	err := servConf.initServer()
 	if err != nil {
-		log.Println("Init Error: ", err)
+		log.Println("Run server Error: ", err)
 		return err
 	}
 	defer servConf.stopServer()
 
-        log.Println("Server run!")
+	log.Println("Server run!")
 	log.Println("Host: ", servConf.ServerHost)
 	log.Println("Port: ", servConf.ServerPort)
 
@@ -223,6 +338,7 @@ func (servConf *AitHTTPServer) RunHTTPServer() error {
 
 	err = http.ListenAndServe(servConf.ServerHost+":"+servConf.ServerPort, router)
 	if err != nil {
+		log.Println("Run server Error: ", err)
 		return err
 	}
 	return nil
